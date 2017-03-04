210e6c56246b878a8168207001a49d17d8402a5e
maxinakenty <mgreen097@gmail.com>
2017-03-04 14:45:19 +0300

    fixed

--- gulp/tasks/assets.js
+++ gulp/tasks/assets.js
@@ -8,9 +8,11 @@ var $ = require('gulp-load-plugins')();
 var combine = require('stream-combiner2').obj;
 
 gulp.task('assets', function() {
-	return gulp.src(__paths.src.assets, {
-			since: gulp.lastRun('assets')
-		})
+	return gulp.src([
+		'./__src/assets/**',
+		'!./__src/assets/jade{,/**/*}',
+		'!./__src/assets/styles{,/**/*}'
+		])
 		.pipe($.changed(__paths.root.dist))
 		.pipe(gulp.dest(__paths.root.dist));
 });
